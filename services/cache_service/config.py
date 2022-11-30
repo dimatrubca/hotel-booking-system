9785b1a93a4d7f7c2a778dcda75f764782fb3cbc
dimatrubca <dimatrubca@gmail.com>
2022-11-30 16:50:03 +0200

    add sd

--- services/cache_service/config.py
+++ services/cache_service/config.py
@@ -3,8 +3,8 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     DEFAULT_EXPIRATION_TIME = 60
 
-    SERVICE_ID = "cache2"
-    PORT = 8014
+    SERVICE_ID = "cache1"
+    PORT = 8004
 
     SERVICE_DISCOVERY = "http://localhost:8005/"
     SERVICE_DISCOVERY_CACHE_URL = "http://localhost:8005/services/cache"
